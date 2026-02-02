d73aa9b0461cbe63fb717ad7fcf5d8ea290f746d
Caio Souza <souzsdev@gmail.com>
2026-02-01 22:29:15 -0300

    first commit

---
+++ src/main/java/com/souzs/clean_arch_desafio_picpay/CleanArchDesafioPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.souzs.clean_arch_desafio_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchDesafioPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchDesafioPicpayApplication.class, args);
+	}
+
+}
